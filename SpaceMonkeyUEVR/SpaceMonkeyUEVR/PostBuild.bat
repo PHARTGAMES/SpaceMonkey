7b1a8442c0741bbe46ceab8967f1fc2d4be3c4df
PEZZALUCIFER <phartgames@gmail.com>
2025-03-16 13:38:57 +1000

    Add Dakar desert rally profile

--- SpaceMonkeyUEVR/SpaceMonkeyUEVR/PostBuild.bat
+++ SpaceMonkeyUEVR/SpaceMonkeyUEVR/PostBuild.bat
@@ -6,6 +6,7 @@ set TO_DIR_MW="%APPDATA%\UnrealVRMod\MechWarrior-Win64-Shipping\plugins\"
 set TO_DIR_AA="%APPDATA%\UnrealVRMod\ArkAscended\plugins\"
 set TO_DIR_A7="%APPDATA%\UnrealVRMod\Ace7Game\plugins\"
 set TO_DIR_GRIP="%APPDATA%\UnrealVRMod\Grip-Win64-Shipping\plugins\"
+set TO_DIR_DDR="%APPDATA%\UnrealVRMod\Dakar2Game-Win64-Shipping\plugins\"
 
 if "%PLATFORM%"=="Win32" (
 	set TO_DIR_LIBS="..\Release\Win32"
@@ -23,11 +24,13 @@ copy /Y "%FROM_DIR%SpaceMonkeyUEVR%PLT%.dll" %TO_DIR_TO%
 copy /Y "%FROM_DIR%SpaceMonkeyUEVR%PLT%.dll" %TO_DIR_AA%
 copy /Y "%FROM_DIR%SpaceMonkeyUEVR%PLT%.dll" %TO_DIR_A7%
 copy /Y "%FROM_DIR%SpaceMonkeyUEVR%PLT%.dll" %TO_DIR_GRIP%
+copy /Y "%FROM_DIR%SpaceMonkeyUEVR%PLT%.dll" %TO_DIR_DDR%
 copy /Y "%FROM_DIR%SpaceMonkeyTelemetryAPI%PLT%.dll" %TO_DIR_MW%
 copy /Y "%FROM_DIR%SpaceMonkeyTelemetryAPI%PLT%.dll" %TO_DIR_TO%
 copy /Y "%FROM_DIR%SpaceMonkeyTelemetryAPI%PLT%.dll" %TO_DIR_AA%
 copy /Y "%FROM_DIR%SpaceMonkeyTelemetryAPI%PLT%.dll" %TO_DIR_A7%
 copy /Y "%FROM_DIR%SpaceMonkeyTelemetryAPI%PLT%.dll" %TO_DIR_GRIP%
+copy /Y "%FROM_DIR%SpaceMonkeyTelemetryAPI%PLT%.dll" %TO_DIR_DDR%
 
 copy /Y "%FROM_DIR%SpaceMonkeyTelemetryAPI%PLT%.dll" "..\..\GenericTelemetryProvider\Release"
 
